f1ceefea283db26d9fd4867a76fe28e3c335f1e9
Rythmere <rythmere@gmail.com>
2022-05-22 21:21:55 -0500

    Task 4.7

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ import './index.css';
 import App from './App';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
+import * as atatus from 'atatus-spa';
+atatus.config('2a4f77e8b887457fae5f502459ff7d75').install();
 
 ReactDOM.render(
   <React.StrictMode>
--- src/numberOfEvents.js
+++ src/numberOfEvents.js
@@ -1,18 +1,37 @@
 import React, { Component } from 'react';
+import { ErrorAlert } from './Alert';
 
 class NumberOfEvents extends Component {
+    state = {
+        errorText:''
+    }
 
     handleInputChanged = (event) => {
         const value = event.target.value;
-        this.props.updateNumberOfEvents(value);
+        if(value > 32 || value <= 0) {
+            this.setState({
+                errorText: 'Enter a number between 1 and 32'
+            });
+        }else {
+            this.props.updateNumberOfEvents(value);
+            this.setState({
+                errorText: ''
+            });
+        }
+        
     }
     render() {
-        return <input
+        return ( 
+            <div className='numberOfEvents'>
+                
+                <input
                     type="text"
-                    className='numberOfEvents'
-                    value= {this.props.numberOfEvents}
+                    
                     onChange={this.handleInputChanged}
                 />
+                <ErrorAlert text={this.state.errorText} />
+            </div>
+        );
     }
 }
 
