ff99c4818e43ad8c2c6734eded2603076c518617
Rythmere <rythmere@gmail.com>
2022-05-24 19:32:50 -0500

    task 4.9

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import './App.css';
 import './nprogress.css';
+import { ScatterChart, Scatter, XAxis, YAxis, CartesianGrid, Tooltip, ResponsiveContainer } from 'recharts';
 import EventList from './EventList';
 import CitySearch from './citySearch';
 import NumberOfEvents from './numberOfEvents';
 import WelcomeScreen from './WelcomeScreen';
 import { extractLocations, getEvents, checkToken, getAccessToken } from './api';
 import { OfflineAlert } from './Alert';
+import EventGenre from './eventGenre';
 
 
 class  App extends Component {
@@ -22,17 +24,20 @@ class  App extends Component {
   async componentDidMount() {
     this.mounted = true;
     const accessToken = localStorage.getItem('access_token');
-    const isTokenValid = (await checkToken(accessToken)).error ? false : true;
+    const isTokenValid = !(accessToken && !navigator.onLine) && (await checkToken(accessToken)).error ? false : true;
     const searchParams = new URLSearchParams(window.location.search);
     const code = searchParams.get("code");
-    this.setState({ showWelcomeScreen: !(code || isTokenValid) });
-    if ((code || isTokenValid) && this.mounted) {
+    const byPassWelcomeScreen = code || isTokenValid;
+
+    this.setState({ showWelcomeScreen: !byPassWelcomeScreen });
+    if (byPassWelcomeScreen && this.mounted) {
       getEvents().then((events) => {
       if (this.mounted) {
-      this.setState({ events, locations: extractLocations(events) });
+      let eventsRendered = this.state.numberOfEvents
+      this.setState({ events:events.slice(0, eventsRendered), locations: extractLocations(events) });
       }
     });
-  }
+  } 
 
     if(!navigator.onLine) {
       this.setState({offlineText: 'There is no internet connection, events may not be up to date.'}); 
@@ -65,14 +70,42 @@ class  App extends Component {
       });
     });
   }
+
+  getData = () => {
+    const {locations, events} = this.state;
+    const data = locations.map((location) =>{
+      const number = events.filter((event) => event.location === location).length
+      const city = location.split(', ').shift()
+      return {city, number};
+    })
+    return data;
+  };
+
   render(){
     if (this.state.showWelcomeScreen === undefined) return <div
 className="App" />
   return (
     <div className="App">
-      <OfflineAlert text={this.state.offlineText} /> 
-      <CitySearch locations={this.state.locations} updateEvents={this.updateEvents}/>
+      <OfflineAlert text={this.state.offlineText} />
+      <h1>Meet App</h1>
+      <CitySearch locations={this.state.locations} updateEvents={this.updateEvents} numberOfEvents={this.state.numberOfEvents} />
       <NumberOfEvents numberOfEvents={this.state.numberOfEvents} updateNumberOfEvents={this.updateNumberOfEvents} />
+      <div className='data-vis-wrapper'>
+      <EventGenre events={this.state.events} />
+      <ResponsiveContainer height={400}>
+      <ScatterChart
+          margin={{
+            top: 20, right: 20, bottom: 20, left: 20,
+          }}
+        >
+          <CartesianGrid />
+          <XAxis type="category" dataKey="city" name="city" />
+          <YAxis type="number" dataKey="number" name="number of events" />
+          <Tooltip cursor={{ strokeDasharray: '3 3' }} />
+          <Scatter data={this.getData()} fill="#8884d8" />
+        </ScatterChart>
+        </ResponsiveContainer>
+        </div>
       <EventList events={this.state.events}/>
       <WelcomeScreen showWelcomeScreen={this.state.showWelcomeScreen} getAccessToken={() => { getAccessToken() }} />
     </div>
--- src/Event.js
+++ src/Event.js
@@ -14,13 +14,13 @@ class Event extends Component {
         const {event} = this.props;
         const {collapsed} = this.state;
         return <div className='event'>
-            <p className='summary'>{event.summary}</p>
-            <p className='location'>{event.location}</p>
-            <p className='date'>{event.start.dateTime} ({event.start.timeZone})</p>
+            <h3 className='summary'>{event.summary}</h3>
+            <h4 className='location'>{event.location}</h4>
+            <h4 className='date'>{event.start.dateTime} ({event.start.timeZone})</h4>
             {collapsed && <button className='show-details details-btn' onClick={this.handleClick}>Show Details</button> }
             {!collapsed &&
                 <div className='hiddenDetails'>
-                    <p>About Event</p>
+                    <h5>About Event</h5>
                     <a className='calendarLink' href={event.htmlLink} target='_blank'>See event details on Google Calendar</a>
                     <p className='description'>{event.description}</p>
                     <button className='hideDetails details-btn' onClick={this.handleClick}>Hide Details</button>
